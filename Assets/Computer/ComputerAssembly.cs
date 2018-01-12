afdf68ae887244395132d2cd3a28f1ee5aec93d0
Pavel Vorobyov <aquavitale@yandex.ru>
2018-01-13 00:35:39 +0300

    Control Panel started

--- Assets/Computer/ComputerAssembly.cs
+++ Assets/Computer/ComputerAssembly.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(ComputerMemory))]
 [RequireComponent(typeof(ComputerTimerDevice))]
@@ -21,8 +22,32 @@ public class ComputerAssembly : MonoBehaviour {
     public double cpuFrequency = 8.0f;
     public bool stopped = false;
 
+    string[] registerNames =
+    {
+        "PC",
+        "A0",
+        "A1",
+        "A2",
+        "A3",
+        "A4",
+        "A5",
+        "A6",
+        "A7",
+        "D0",
+        "D1",
+        "D2",
+        "D3",
+        "D4",
+        "D5",
+        "D6",
+        "D7",
+    };
+
+    Dictionary<string, Text> uiRegisterMap;
+
     void Start()
     {
+        BuildRegisterMap();
         mem = GetComponent<ComputerMemory>();
         timer = GetComponent<ComputerTimerDevice>();
         Cpu.Init();
@@ -41,6 +66,22 @@ public class ComputerAssembly : MonoBehaviour {
         executionThreadStart = new ThreadStart(CpuExecutionThread);
         executionThread = new Thread(executionThreadStart);
         Invoke("StartEmulation", 0.1f);
+    }
+
+    private void BuildRegisterMap()
+    {
+        uiRegisterMap = new Dictionary<string, Text>();
+        Text[] objects = FindObjectsOfType<Text>();
+        foreach (Text go in objects)
+        {
+            foreach (string regName in registerNames)
+            {
+                if (go.name == regName)
+                {
+                    uiRegisterMap[regName] = go;
+                }
+            }
+        }
     }
 
     void StartEmulation()
@@ -71,6 +112,23 @@ public class ComputerAssembly : MonoBehaviour {
         if (initiated)
         {
             registers = Cpu.Registers;
+            uiRegisterMap["PC"].text = string.Format("PC 0x{0:X8}", registers.PC);
+            uiRegisterMap["A0"].text = string.Format("A0 0x{0:X8}", registers.A0);
+            uiRegisterMap["A1"].text = string.Format("A1 0x{0:X8}", registers.A1);
+            uiRegisterMap["A2"].text = string.Format("A2 0x{0:X8}", registers.A2);
+            uiRegisterMap["A3"].text = string.Format("A3 0x{0:X8}", registers.A3);
+            uiRegisterMap["A4"].text = string.Format("A4 0x{0:X8}", registers.A4);
+            uiRegisterMap["A5"].text = string.Format("A5 0x{0:X8}", registers.A5);
+            uiRegisterMap["A6"].text = string.Format("A6 0x{0:X8}", registers.A6);
+            uiRegisterMap["A7"].text = string.Format("A7 0x{0:X8}", registers.A7);
+            uiRegisterMap["D0"].text = string.Format("D0 0x{0:X8}", registers.D0);
+            uiRegisterMap["D1"].text = string.Format("D1 0x{0:X8}", registers.D1);
+            uiRegisterMap["D2"].text = string.Format("D2 0x{0:X8}", registers.D2);
+            uiRegisterMap["D3"].text = string.Format("D3 0x{0:X8}", registers.D3);
+            uiRegisterMap["D4"].text = string.Format("D4 0x{0:X8}", registers.D4);
+            uiRegisterMap["D5"].text = string.Format("D5 0x{0:X8}", registers.D5);
+            uiRegisterMap["D6"].text = string.Format("D6 0x{0:X8}", registers.D6);
+            uiRegisterMap["D7"].text = string.Format("D7 0x{0:X8}", registers.D7);
             interruptsRequested = Cpu.interruptsRequested;
         }
     }
---
+++ Assets/ControlPanel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPanel : MonoBehaviour {
+
+    public float widthPercentage = 0.4f;
+
+    bool expanded = false;
+    RectTransform rt;
+    RectTransform parent;
+	// Use this for initialization
+	void Start () {
+        Canvas cvs = GetComponentInParent<Canvas>();
+        parent = cvs.GetComponent<RectTransform>();
+        rt = GetComponent<RectTransform>();
+
+        float width = Mathf.Clamp(parent.sizeDelta.x * widthPercentage, 280f, 350f);
+        float height = parent.sizeDelta.y;
+        float x = parent.sizeDelta.x / 2;
+        rt.sizeDelta = new Vector2(width, height);
+        rt.localPosition = new Vector3(x, 0, 0);
+    }
+
+    void Update () {
+        float x;
+        float width = Mathf.Clamp(parent.sizeDelta.x * widthPercentage, 280f, 350f);
+        float height = parent.sizeDelta.y;
+        if (expanded)
+        {
+            x = parent.sizeDelta.x / 2;
+        }
+        else
+        {
+            x = parent.sizeDelta.x / 2 + width;
+        }
+        rt.sizeDelta = new Vector2(width, height);
+
+        Vector3 currentPosition = rt.localPosition;
+        rt.localPosition = Vector3.Lerp(currentPosition, new Vector3(x, 0, 0), 0.25f);
+
+        if (Input.GetKeyDown(KeyCode.Pause))
+        {
+            expanded = !expanded;
+        }
+    }
+}
